a2dc6bec634c7da2c89b3b6f03ae9a36c6f6b820
h3nnn4n <uber.renan@gmail.com>
2020-10-03 13:07:06 -0300

    Add support for query filters

--- git_o_matic_9k/settings.py
+++ git_o_matic_9k/settings.py
@@ -133,9 +133,11 @@ STATIC_URL = '/static/'
 # Django Rest Framework Settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
+
 
 # Celery settings
 
