db450a683587dde86fa140e609ff108715ba8a53
h3nnn4n <uber.renan@gmail.com>
2020-10-03 11:23:31 -0300

    Improve code reusability

---
+++ github/services.py
@@ -0,0 +1,63 @@
+from .models import Developer, Repository
+
+
+def add_or_update_user(data):
+    fields = [
+        'login',
+        'name',
+        'location',
+        'bio',
+        'company',
+        'email',
+
+        'created_at',
+        'updated_at',
+
+        'followers',
+        'following',
+        'public_gists',
+        'public_repos',
+    ]
+
+    defaults = { field: data[field] for field in fields }
+    defaults['data_source'] = data
+
+    Developer.objects.update_or_create(
+        github_id=data['id'],
+        defaults=defaults
+    )
+
+
+def add_or_update_repository(data):
+    fields = [
+        'name',
+        'full_name',
+        'description',
+        'homepage',
+        'language',
+        'created_at',
+        'updated_at',
+
+        'has_downloads',
+        'has_issues',
+        'has_pages',
+        'has_projects',
+        'has_wiki',
+        'private',
+        'archived',
+        'disabled',
+
+        'stargazers_count',
+        'subscribers_count',
+        'watchers_count',
+        'open_issues_count',
+    ]
+
+    defaults = { field: data[field] for field in fields }
+    defaults['data_source'] = data
+    defaults['owner'] = Developer.objects.get(github_id=data['owner']['id'])
+
+    Repository.objects.update_or_create(
+        github_id=data['id'],
+        defaults=defaults
+    )
--- github/tasks.py
+++ github/tasks.py
@@ -1,79 +1,27 @@
 from celery import shared_task
 
+from . import services
+
 from .models import Developer, Repository
 from . import github_api
 
 
 @shared_task
 def add_or_update_user(user_name):
-    result = github_api.get_user(user_name)
+    dev_data = github_api.get_user(user_name)
 
-    fields = [
-        'login',
-        'name',
-        'location',
-        'bio',
-        'company',
-        'email',
-
-        'created_at',
-        'updated_at',
-
-        'followers',
-        'following',
-        'public_gists',
-        'public_repos',
-    ]
-
-    defaults = { field: result[field] for field in fields }
-    defaults['data_source'] = result
-
-    Developer.objects.update_or_create(
-        github_id=result['id'],
-        defaults=defaults
-    )
+    services.add_or_update_user(dev_data)
 
 
 @shared_task
 def add_or_update_repository(repo_name):
-    result = github_api.get_repository(repo_name)
-
-    fields = [
-        'name',
-        'full_name',
-        'description',
-        'homepage',
-        'language',
-        'created_at',
-        'updated_at',
-
-        'has_downloads',
-        'has_issues',
-        'has_pages',
-        'has_projects',
-        'has_wiki',
-        'private',
-        'archived',
-        'disabled',
-
-        'stargazers_count',
-        'subscribers_count',
-        'watchers_count',
-        'open_issues_count',
-    ]
-
-    defaults = { field: result[field] for field in fields }
-    defaults['data_source'] = result
+    repo_data = github_api.get_repository(repo_name)
 
     try:
-        defaults['owner'] = Developer.objects.get(github_id=result['owner']['id'])
+        services.add_or_update_repository(repo_data)
     except Developer.DoesNotExist:
         # If the user doesnt exist yet, create it
         user_name = repo_name.split('/')[0]
         add_or_update_user.run(user_name)
-        defaults['owner'] = Developer.objects.get(github_id=result['owner']['id'])
 
-    Repository.objects.update_or_create(
-        github_id=result['id'],
-        defaults=defaults
-    )
+    services.add_or_update_repository(repo_data)
