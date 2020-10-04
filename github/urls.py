686ee6447dd59408f0b0268a4143a9082692500c
h3nnn4n <uber.renan@gmail.com>
2020-10-04 15:38:20 -0300

    Add drf-yasg for swagger/redoc generation

--- github/urls.py
+++ github/urls.py
@@ -1,6 +1,24 @@
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.urls import include, path
-from rest_framework import routers
+from django.conf.urls import url
+from rest_framework import routers, permissions
+
 from . import views
+
+# pylint: disable: invalid-name
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'developers', views.DeveloperViewSet)
@@ -18,4 +36,7 @@ urlpatterns = [
     path('trigger_discovery/',
          views.discovery_scraper,
          name='discovery_scraper'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
