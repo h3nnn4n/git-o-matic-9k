8fb0b7ddf0336e85e1b0f314ea81b9eeca79fc06
h3nnn4n <uber.renan@gmail.com>
2020-10-04 10:05:11 -0300

    Revert "Simple hack to crawl users from a starting user"
    This reverts commit 4a0367f17e71cfbced1d604d041f35b19e0988c6.
    
    It was just a test. it is not a production safe code

--- github/tasks.py
+++ github/tasks.py
@@ -1,6 +1,3 @@
-from datetime import timedelta
-from django.utils import timezone
-
 from celery import shared_task
 
 from . import services
@@ -20,10 +17,8 @@ def add_or_update_user(user_name):
         return
 
     dev_data = github_api.get_user(user_name)
-    services.add_or_update_user(dev_data)
 
-    eta = timezone.now() + timedelta(seconds=45)
-    add_or_update_user_followers.apply_async(args=[user_name], eta=eta)
+    services.add_or_update_user(dev_data)
 
 
 @shared_task
